02c59c294ba588a433098d192166f64410e95160
torotzl <estiven.toro046@pascualbravo.edu.co>
2024-08-26 04:30:31 -0500

    Formulario
    Actividad completa de formulario, diseño gráfico y java.

--- DispositivosMoviles2024-2/app/src/main/java/com/example/clase2/MainActivity.java
+++ DispositivosMoviles2024-2/app/src/main/java/com/example/clase2/MainActivity.java
@@ -2,41 +2,93 @@ package com.example.clase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText usuario; //Declarar componentes
-    TextView textView;
-    Button btnjuanito;
-    Button btnAlimaña;
-    String Usuario;
+    LinearLayout llButtons;
+    TextView tvSucc;
+    EditText etUser;
+    EditText etPass;
+    Button btnAccept;
+    Button btnClear;
+    String TAG = "Test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usuario = (EditText) findViewById(R.id.usuario); //Comprobar el "Tipo de Dato" del componente con su id
-        textView = (TextView) findViewById(R.id.textview);
-        btnjuanito = (Button) findViewById(R.id.btnJuanito);
-        btnAlimaña = (Button) findViewById(R.id.btnAlimaña);//No es necesario poner el tipo de dato en parentesis, pero con este se evitan problemas con id iguales
+        llButtons = (LinearLayout) findViewById(R.id.llButtons);
+        tvSucc = (TextView) findViewById(R.id.tvSucc);
+        etUser = (EditText) findViewById(R.id.etUser);
+        etPass = (EditText) findViewById(R.id.etPass);
+        btnAccept = (Button) findViewById(R.id.btnAccept);
+        btnClear = (Button) findViewById(R.id.btnClear);
 
-        btnjuanito.setOnClickListener(new View.OnClickListener() {
+        Log.d(TAG,"OnCreate");
+
+        btnAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText("Ome Recocha ome");
-                Usuario = String.valueOf(usuario.getText());
+                tvSucc.setText("Juanito Alimaña");
+                etUser.setText("Listo");
+                etPass.setText("Calisto");
+                llButtons.setBackgroundColor(getResources().getColor(R.color.sky_blue));
+                tvSucc.setBackgroundColor(getResources().getColor(R.color.black));
             }
         });
-        btnAlimaña.setOnClickListener(new View.OnClickListener() {
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                etUser.setText("");
+                etPass.setText("");
+                tvSucc.setText("Limpio :D");
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"OnResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"OnDestroy");
     }
 }
