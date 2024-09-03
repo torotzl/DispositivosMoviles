6470d6758dfbb104062b06dc4f910285c6117a31
torotzl <estiven.toro046@pascualbravo.edu.co>
2024-09-02 21:33:34 -0500

    Splash

---
+++ DispositivosMoviles2024-2/app/src/main/java/com/example/clase2/Splash.java
@@ -0,0 +1,24 @@
+package com.example.clase2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash  = new Intent(Splash.this, MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },3000);
+    }
+}
